60312d3d0516bb4ad7330fcd3d9d572229710f74
Dimon <slatqh@gmail.com>
2019-08-23 18:23:21 -0400

    new post

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import { Post } from './Api/Post';
 
 const App = () => {
   const [posts, setPosts] = useState([]);
-  const [toggle, toggleNewPost] = useState(true);
+  const [toggle, toggleNewPost] = useState(false);
   const [post, postSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
   console.log('App');
