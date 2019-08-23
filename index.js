71678f53237afb623bba94fe063996837905482b
Dimon <slatqh@gmail.com>
2019-08-23 18:36:50 -0400

    initial

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ mongoose.connect(dbUrl, {
   useFindAndModify: false
 });
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Import Routes
 
