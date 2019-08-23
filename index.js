ac1ffcf74f0a59364c82f35c1bfcbefa08e55e1e
Dimon <slatqh@gmail.com>
2019-08-23 19:23:13 -0400

    env

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ if(process.env.NODE_ENV === 'production'){
 // Import Routes
 
 // Start App
+const PORT = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Running at http://localhost:${port}`);
+  console.log(`Running at http://localhost:${PORT}`);
 });
