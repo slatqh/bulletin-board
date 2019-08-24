2a35c2c1755db2e30014c445199c6fd76a347ca4
Dmitriy B <32040388+slatqh@users.noreply.github.com>
2019-08-24 00:42:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -29,12 +29,10 @@ mongoose.connect(dbUrl, {
   useFindAndModify: false
 });
 
-// if(process.env.NODE_ENV === 'production'){
+app.use(express.static(path.join(__dirname, 'client/build')))
+app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
 
-  app.use(express.static(path.join(__dirname, 'client/build')))
-  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
 
- //
 
 // Import Routes
 
